621b098c0cc360ebcab1208ab097e516c9488fe0
Daniel <77182684+JoseDMG2@users.noreply.github.com>
2025-10-19 13:58:06 -0500

    new: mailer.js added and working!

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+app.use(express.json());
+
+export default app;
