3f71762b8a8ead3f6a8b3755f1d38d2d73c848a2
AnrrousUTP <U22233672@utp.edu.pe>
2025-11-06 11:31:09 -0500

    fix: Update MongoDB connection string to use environment variable and modify footer links to navigate correctly

--- Backend/mongoClient.js
+++ Backend/mongoClient.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://Luna:452NITi27J$i@famiglia.19qcnvr.mongodb.net/FamigliaDB?retryWrites=true&w=majority&appName=Famiglia");
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Conexión a MongoDB exitosa");          
     } catch (error) {
         console.error("Error de conexión a MongoDB:", error);
